b4fe64598071385a396b6020c2ffa311323d1016
Edson Sosa <ej_s_12@hotmail.com>
2024-09-04 21:33:13 -0500

    add Estructuras

--- Sesion3/Clase1Base/Program.cs
+++ Sesion3/Clase1Base/Program.cs
@@ -7,5 +7,18 @@ int edad = 20;
 char letra = 'A';
 double dinero = 1543.21;
 
+Console.WriteLine("Escribe tu Nombre:");
+Nombre = Console.ReadLine();
 Console.WriteLine("Tu Nombre es " + Nombre);
+Console.WriteLine("Escribe tu edad");
+edad = int.Parse(Console.ReadLine());
 Console.WriteLine($"Tu edad es {edad}");
+
+if (edad >= 18) 
+{
+    Console.WriteLine("Puedes Ingresar");
+}
+else
+{
+    Console.WriteLine("No puedes Ingresar");
+}
---
+++ Sesion3/Estructura/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingresa el numero limite");
+
+int numeroLimite = int.Parse(Console.ReadLine());
+int contador = 0;
+
+while (contador < numeroLimite)
+{
+    contador++;
+    Console.WriteLine(contador);
+}
+
+Console.WriteLine("DO-WHILE");
+contador = 0;
+do
+{
+    contador++;
+    Console.WriteLine(contador);
+} while (contador < numeroLimite);
+
+Console.WriteLine("MENU DO-WHILE");
+int opcion = 0;
+do
+{
+    Console.WriteLine("Calculadora ---->");
+    Console.WriteLine("1 -Sumar");
+    Console.WriteLine("2 -Restar");
+    Console.WriteLine("3 -Multiplar");
+    Console.WriteLine("4 -Dividir");
+    Console.WriteLine("5 -Potencia");
+    Console.WriteLine("6 -Raiz");
+    Console.WriteLine("7 -Salida");
+
+    Console.WriteLine("Ingresa tu opcion:");
+    opcion = int.Parse(Console.ReadLine());
+    if (opcion == 1)
+        Console.WriteLine("Sumando");
+    if (opcion == 2)
+        Console.WriteLine("Restando");
+} while (opcion != 7);
+
+Console.WriteLine("Salida");
