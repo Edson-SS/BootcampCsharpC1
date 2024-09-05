55486e7aae5f0462a2e628970d1e28ccdd437e81
Edson Sosa <ej_s_12@hotmail.com>
2024-09-04 20:35:49 -0500

    Actualizacion

--- Sesion3/Clase1Base/Program.cs
+++ Sesion3/Clase1Base/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola! Alumnos");
+
+string Nombre = "Edson";
+long altura = 170;
+int edad = 20;
+char letra = 'A';
+double dinero = 1543.21;
+
+Console.WriteLine("Tu Nombre es " + Nombre);
+Console.WriteLine($"Tu edad es {edad}");
