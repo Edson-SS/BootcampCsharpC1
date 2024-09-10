c069b1a077379ce6e1eaa2eb6b9e9d2acdf80c59
Edson Sosa <ej_s_12@hotmail.com>
2024-09-09 23:28:34 -0500

    act

---
+++ Sesion5/Demo03/Program.cs
@@ -0,0 +1,46 @@
+﻿
+void ejemploDoWhile()
+{
+    int valor = 0;
+    do
+    {
+        valor++;
+        Console.WriteLine(valor);
+    } while (valor < 12);
+
+    Console.Read();
+}
+
+void leerNumerosPositivos()
+{
+    int numero = 0;
+    do { 
+        Console.Write("Ingresa el numero positivo: ");
+        numero=Convert.ToInt32(Console.ReadLine());
+    } while (numero <= 0);
+    Console.WriteLine("Numero positivo:" + numero);
+}
+
+void leerNota()
+{
+    int nota = 0;
+    do
+    {
+        Console.Write("Ingresa nota: ");
+        nota = Convert.ToInt32(Console.ReadLine());
+    } while (nota < 0 || nota >20);
+    Console.WriteLine("Su nota es:" + nota);
+}
+
+//int numero;
+//Console.Write("Ingresa un numero: ");
+//numero = Convert.ToInt32( Console.ReadLine());
+
+//Console.Write("El doble del numero es: ");
+//Console.WriteLine(numero*2);
+
+//leerNumerosPositivos();
+
+leerNota();
+
+Console.Read();
